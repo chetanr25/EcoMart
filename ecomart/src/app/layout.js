71e070522cf71cadbc26524fb0b862d8c28e9e40
alfiyafatima09 <alfiyafatima200431@gmail.com>
2025-02-02 07:52:15 +0530

    website done

--- ecomart/src/app/layout.js
+++ ecomart/src/app/layout.js
@@ -1,8 +1,16 @@
 import './globals.css'
 
+export const metadata = {
+  title: 'EcoMart🌿',
+  description: 'Sustainable Shopping Made Easy',
+}
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/favicon.ico" />
+      </head>
       <body suppressHydrationWarning={true}>{children}</body>
     </html>
   );
